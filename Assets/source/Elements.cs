7d45add76c5abdd4cb91cde444e7894c9bb9b652
William Terry <Tezza48is@gmail.com>
2020-04-18 17:37:20 +0100

    Base Game setup

---
+++ Assets/source/Elements.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum Element
+{
+    Hydrogen = 1,
+    Helium = 2,
+    Lithium = 3,
+    Beryllium = 4,
+    // Boron
+    Nitrogen = 6,
+    Carbon = 7,
+    Oxygen = 8,
+    Flourine = 9,
+    Neon = 10,
+}
+
+public class ElementInventory : SortedDictionary<Element, int>
+{
+    public ElementInventory() : base()
+    {
+        
+    }
+
+    public ElementInventory(ElementMakeup[] makeup) : base()
+    {
+        foreach (var element in makeup)
+        {
+            this[element.element] = element.amount;
+        }
+    }
+}
+
+[Serializable]
+public struct ElementMakeup
+{
+    public Element element;
+    public int amount;
+}
+
+public static class Elements
+{
+    public static Dictionary<Element, int> activations;
+
+    public static void Init()
+    {
+        activations = new Dictionary<Element, int>();
+
+        activations[Element.Hydrogen] = 100;
+        activations[Element.Helium] = 200;
+        activations[Element.Lithium] = 300;
+        activations[Element.Beryllium] = 350;
+
+        activations[Element.Nitrogen] = 1000;
+        activations[Element.Carbon] = 1200;
+        activations[Element.Oxygen] = 3000;
+    }
+}
---
+++ Assets/source/Star.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public ElementMakeup[] initialConditions;
+    public ElementInventory composition;
+
+    float tTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Elements.Init();
+
+        composition = new ElementInventory(initialConditions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mass = GetTotalMass();
+        var scale = mass / 100.0f;
+        transform.localScale = new Vector3(scale, scale, scale);
+
+        // Every second
+        if (tTime > Time.time)
+        {
+            return;
+        }
+
+        tTime = Time.time + 1.0f;
+        float fusionChance = 0.01f;
+
+        var deltas = new ElementInventory();
+
+        foreach (var comp in composition)
+        {
+            var element = comp.Key;
+            var count = comp.Value;
+
+            if (Elements.activations[element] < mass)
+            {
+                var selfFusionProduct = (int)element * 2;
+                if (Enum.IsDefined(typeof(Element), selfFusionProduct))
+                {
+                    var product = (Element)selfFusionProduct;
+
+                    var numToFuse = Mathf.CeilToInt(count * fusionChance);
+
+                    if (!deltas.ContainsKey(element)) deltas[element] = 0;
+                    if (!deltas.ContainsKey(product)) deltas[product] = 0;
+
+                    deltas[element] -= numToFuse * 2;
+                    deltas[product] += numToFuse;
+                }
+
+                var hFusionProduct = (int)element + 1;
+                if (Enum.IsDefined(typeof(Element), hFusionProduct) && UnityEngine.Random.value < 0.5)
+                {
+                    var product = (Element)hFusionProduct;
+
+                    var numToFuse = Mathf.CeilToInt(count * fusionChance / 10);
+
+                    if (!deltas.ContainsKey(element)) deltas[element] = 0;
+                    if (!deltas.ContainsKey(product)) deltas[product] = 0;
+
+                    deltas[element] -= numToFuse * 2;
+                    deltas[product] += numToFuse;
+                }
+
+            }
+        }
+
+        AddElements(deltas);
+    }
+
+    public void AddElements(ElementInventory inventory)
+    {
+        foreach (var elem in inventory)
+        {
+            var name = elem.Key;
+
+            if (!composition.ContainsKey(name)) composition[name] = 0;
+            composition[name] = Math.Max(composition[name] + elem.Value, 0);
+        }
+    }
+
+    public int GetTotalMass()
+    {
+        int mass = 0;
+        foreach(var element in composition)
+        {
+            mass += (int)element.Key * element.Value;
+        }
+
+        return mass;
+    }
+
+    #region GUI
+    struct GuiSettings
+    {
+        public Vector2 scrollPos;
+    };
+
+    GuiSettings guiSettings;
+
+    private void GUIWindowFunc(int windowId)
+    {
+        guiSettings.scrollPos = GUILayout.BeginScrollView(guiSettings.scrollPos);
+        GUILayout.Label("Total mass: " + GetTotalMass());
+
+        GUILayout.Label("Composition");
+        GUILayout.BeginVertical();
+        foreach(var elem in composition)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(elem.Key.ToString() + ((int)elem.Key).ToString());
+            GUILayout.Label(elem.Value.ToString());
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndScrollView();
+    }
+
+    private void OnGUI()
+    {
+        Rect r = new Rect(0, 0, 200, 400);
+        GUILayout.Window(1, r, GUIWindowFunc, "Star Info");
+    }
+    #endregion
+}
